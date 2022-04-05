6d9bbb658a4f6e84a6d5b91fa9aa95ef1d1ea937
Anurag Gupta <anurag@healofy.com>
2022-04-06 03:34:14 +0530

    Fix the menu Issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,23 +3,23 @@
 	"use strict";
 
 
-	$('.owl-carousel').owlCarousel({
-		loop: true,
-		margin: 30,
-		nav: true,
-		pagination: true,
-		responsive: {
-			0: {
-				items: 1
-			},
-			600: {
-				items: 1
-			},
-			1000: {
-				items: 2
-			}
-		}
-	})
+	// $('.owl-carousel').owlCarousel({
+	// 	loop: true,
+	// 	margin: 30,
+	// 	nav: true,
+	// 	pagination: true,
+	// 	responsive: {
+	// 		0: {
+	// 			items: 1
+	// 		},
+	// 		600: {
+	// 			items: 1
+	// 		},
+	// 		1000: {
+	// 			items: 2
+	// 		}
+	// 	}
+	// })
 
 
 	$(window).scroll(function () {
